1387282503c7ff27913f875d8977bc4ee44d7505
Lucas <tuga@vivaldi.net>
2021-03-16 23:11:34 +0100

    Test: Slight modifications on the filter and dropDown for implementation tests

---
+++ MTG_Decks/Shared/Model/ResponseCardType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTG_Decks.Shared.Model
+{ 
+	public class ResponseCardType
+	{
+		public string Object { get; set; }
+		public string URI { get; set; }
+		public int Total_values { get; set; }
+		public List<string> Data { get; set; }
+	}
+}
